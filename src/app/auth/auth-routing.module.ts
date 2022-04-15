8b2665f29a26bf3f1f535a59f21afc5750d831d6
Angel Uzcategui <angeluz142@gmail.com>
2022-04-15 03:47:28 -0300

    agregado material flexlayout y creacion de landing

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,40 @@
+// modules
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+// components
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+const routes:Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:'login',
+        component:LoginComponent
+      },
+      {
+        path:'registro',
+        component:RegistroComponent
+      },
+      {
+        path:'**',
+        redirectTo:'login'
+      }
+    ]
+  }
+]
+
+
+@NgModule({
+  
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
